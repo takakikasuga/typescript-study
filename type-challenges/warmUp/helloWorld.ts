b0986ea13a9dca1e45fdb8f1c5fc8b0fe82bd144
takakikasuga <takaki.soccer1@gmail.com>
2022-06-04 20:23:49 +0900

    :sparkles: initial commit

---
+++ type-challenges/warmUp/helloWorld.ts
@@ -0,0 +1,8 @@
+/* _____________ Your Code Here _____________ */
+
+type HelloWorld = string; // expected to be a string
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, NotAny } from '@type-challenges/utils';
+
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
